c6da53d707ebe55c2da118d3af16cbe67322e1de
Stijn <shogerwerf.gaming@gmail.com>
2024-04-24 10:36:12 +0200

    Added showing and not showing of tree

---
+++ Assets/Scripts/R&D Tree/RDTreeManager.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class RDTreeManager : MonoBehaviour
+{
+    [SerializeField] private GameObject _skillTree;
+
+    private RDControls _controls;
+
+    public static RDTreeManager Instance;
+
+    public List<GameObject> Skills { get; private set; }
+
+    private void OnEnable()
+    {
+        _controls.RDControlls.ShowTree.performed += ShowTree;
+    }
+
+    private void OnDisable()
+    {
+        _controls.RDControlls.ShowTree.performed -= ShowTree;
+    }
+
+    private void Awake() 
+    {
+        if(Instance == null) Instance = this;
+        else Destroy(Instance);
+
+        _controls = new RDControls();
+        _controls.Enable();
+    }
+
+    private void ShowTree(InputAction.CallbackContext _) => _skillTree.SetActive(!_skillTree.activeSelf);
+
+}
