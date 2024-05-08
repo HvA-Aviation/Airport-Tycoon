efd498eae039bf18b8e63e2a80de50488b9abac1
Stijn <shogerwerf.gaming@gmail.com>
2024-05-08 10:57:36 +0200

    Set the child position so that it is always ontop of other menus

--- Assets/Scripts/MenuLayerSystem/EnableUI.cs
+++ Assets/Scripts/MenuLayerSystem/EnableUI.cs
@@ -1,15 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class EnableUI : MonoBehaviour
 {
     [SerializeField] private KeyCode _dissableEnableScreen;
     [SerializeField] private GameObject _menuGFX;
 
+    public UnityEvent PressEnableUIButton = new UnityEvent();
+
+    private void Awake()
+    {
+        PressEnableUIButton.AddListener(ActivateUI);
+        PressEnableUIButton.AddListener(SetChildPos);
+    }
+
     private void Update()
     {
         if (Input.GetKeyDown(_dissableEnableScreen))
-            _menuGFX.SetActive(!_menuGFX.activeSelf);
+            PressEnableUIButton.Invoke();
     }
+
+    /// <summary>
+    /// When the activate button is pressed the UI will be shown
+    /// </summary>
+    private void ActivateUI() => _menuGFX.SetActive(!_menuGFX.activeSelf);
+    
+    /// <summary>
+    /// Set the childpos to the last of the children so it is ontop of everything
+    /// </summary>
+    private void SetChildPos() => transform.SetAsLastSibling();
+    
 }
