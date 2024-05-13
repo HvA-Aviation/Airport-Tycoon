19201ef228f404ef081a53cbde1e7af4fb89ae8c
Tom Jonker <tjj.jonker@gmail.com>
2024-05-13 11:00:36 +0200

    Added more docs

--- Assets/TaskSystem/Worker.cs
+++ Assets/TaskSystem/Worker.cs
@@ -1,13 +1,27 @@
 using UnityEngine;
 
+/// <summary>
+/// Base class for a worker.
+/// </summary>
 public abstract class Worker : MonoBehaviour
 {
+    /// <summary>
+    /// Possible states of the worker.
+    /// </summary>
     public enum WorkerState
     {
         Busy,
         Free
     }
 
+    /// <summary>
+    /// Current state of the worker.
+    /// </summary>
     public WorkerState State { get; private set; }
+
+    /// <summary>
+    /// Sets the state of the worker.
+    /// </summary>
+    /// <param name="state">State of the worker.</param>
     public void SetState(WorkerState state) {  State = state; }
 }
