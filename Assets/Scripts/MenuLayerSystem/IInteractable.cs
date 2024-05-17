fc4011d75f51f945837cce2ebc9715047f1fb211
Hotoroti <shogerwerf.work@gmail.com>
2024-05-17 13:51:23 +0200

    Added summaries

--- Assets/Scripts/MenuLayerSystem/IInteractable.cs
+++ Assets/Scripts/MenuLayerSystem/IInteractable.cs
@@ -1,11 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public interface IInteractable
 {
+    /// <summary>
+    /// This function will be called when the interaction of the object needs to be dissabled
+    /// </summary>
     public void DissableInteraction();
 
+    /// <summary>
+    /// This function will be called when the interaction of the object needs to be enabled
+    /// </summary>
     public void EnableInteraction();
 
 }
