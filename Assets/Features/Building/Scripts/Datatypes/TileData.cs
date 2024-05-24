8070dcb26da35dbe360a113b20118e26ec61f623
codyb <cody.bolleboom@gmail.com>
2024-05-24 12:32:20 +0200

    Added Utiltiy load

--- Assets/Features/Building/Scripts/Datatypes/TileData.cs
+++ Assets/Features/Building/Scripts/Datatypes/TileData.cs
@@ -7,9 +7,11 @@ namespace Features.Building.Scripts.Datatypes
     [Serializable]
     public class TileData
     {
-        [field: SerializeField] public Tile Tile { get; private set;  }
+        [field: SerializeField] public Tile Tile { get; private set; }
         public float WorkLoad;
         public bool UnTraversable = false;
-        public UtilityType UtilityType;
+
+        [Header("Utility Settings")] public UtilityType UtilityType;
+        public float UtilityLoad;
     }
 }
