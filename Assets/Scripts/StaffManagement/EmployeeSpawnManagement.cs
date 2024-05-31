b84e1b073db368eb94a97f2be5d52d151738c6f6
Hotoroti <shogerwerf.work@gmail.com>
2024-05-31 15:40:16 +0200

    Fixed to spawn different objects

--- Assets/Scripts/StaffManagement/EmployeeSpawnManagement.cs
+++ Assets/Scripts/StaffManagement/EmployeeSpawnManagement.cs
@@ -1,21 +1,31 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EmployeeSpawnManagement : MonoBehaviour
 {
-    [SerializeField] private GameObject _employeeOBJ;
+    [SerializeField] private List<EmployeeObjects> _employees = new List<EmployeeObjects>();
+
+    private Dictionary<Employee.EmployeeTypes, GameObject> _employeeObjects = new Dictionary<Employee.EmployeeTypes, GameObject>();
 
     private GameObject _parentOBJ;
 
+    private void Awake()
+    {
+        foreach (EmployeeObjects employeeOBJ in _employees)
+            _employeeObjects.Add(employeeOBJ.Type, employeeOBJ.Object);
+    }
+
     private void Start() => _parentOBJ = new GameObject("EmployeesParent");
 
-   /// <summary>
-   /// This function will return an employee that is instantiated of the type of employee that is spawned
-   /// </summary>
-   /// <param name="type">The type of employee that needs to be spawned</param>
-   /// <returns>The Employee of the type of employee</returns>
+    /// <summary>
+    /// This function will return an employee that is instantiated of the type of employee that is spawned
+    /// </summary>
+    /// <param name="type">The type of employee that needs to be spawned</param>
+    /// <returns>The Employee of the type of employee</returns>
     public Employee InstantiateEmployee(Employee.EmployeeTypes type)
     {
-        return Instantiate(_employeeOBJ, Vector3.zero, Quaternion.identity, _parentOBJ.transform).GetComponent<Employee>();        
+        return Instantiate(_employeeObjects[type], Vector3.zero, Quaternion.identity, _parentOBJ.transform).GetComponent<Employee>();
     }
 
     /// <summary>
@@ -25,5 +35,12 @@ public class EmployeeSpawnManagement : MonoBehaviour
     public void DespawnEmployeeOBJ(GameObject employeeOBJ)
     {
         Destroy(employeeOBJ);
+    }
+
+    [Serializable]
+    public struct EmployeeObjects
+    {
+        public Employee.EmployeeTypes Type;
+        public GameObject Object;
     }
 }
