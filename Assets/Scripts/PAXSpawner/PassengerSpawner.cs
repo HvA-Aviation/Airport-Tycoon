155650c54747bd0c8423203a9cfb213e94b82f03
Hotoroti <shogerwerf.work@gmail.com>
2024-05-31 10:27:31 +0200

    Changed spawneer to a manager and a seperate script

--- Assets/Scripts/PAXSpawner/PassengerSpawner.cs
+++ Assets/Scripts/PAXSpawner/PassengerSpawner.cs
@@ -4,26 +4,31 @@ using UnityEngine;
 
 public class PassengerSpawner : MonoBehaviour
 {
-    [SerializeField] private float _spawnRate;
     [SerializeField] private bool _canSpawnPassengerOnTime;
-    public float SpawnRate => _spawnRate;
-
+    [SerializeField] private PassengerSpawnManager _spawnerManager;
     private void Start()
     {
         StartCoroutine(SpawnPassengerOnTimer());
     }
 
+    /// <summary>
+    /// Call this function when you want to spawn an passenger
+    /// </summary>
     public void SpawnPassenger()
     {
         GameObject temp = GameManager.Instance.PoolManager.AllObjectPools[0].GetObject();
         temp.transform.position = transform.position;
     }
 
+    /// <summary>
+    /// This IEnumerator will spawn passengers on a time basis
+    /// </summary>
+    /// <returns></returns>
     public IEnumerator SpawnPassengerOnTimer()
     {
         while (_canSpawnPassengerOnTime)
         {
-            yield return new WaitForSeconds(SpawnRate);
+            yield return new WaitForSeconds(_spawnerManager.SpawnRate);
 
             SpawnPassenger();
         }
