03326954e064f7db3b34d3d89c77302ad173b8e7
Stijn <shogerwerf.gaming@gmail.com>
2024-06-12 09:47:06 +0200

    Added UI back to scene and update GameTimeManager

--- Assets/Features/Managers/GameTimeManager.cs
+++ Assets/Features/Managers/GameTimeManager.cs
@@ -4,22 +4,18 @@ namespace Features.Managers
 {
     public class GameTimeManager : MonoBehaviour
     {
-        [SerializeField] private float _timeScale = 1;
+        [SerializeField] private float _defaultTimeScale = 1;
 
-        public float TimeScale
-        {
-            get { return _timeScale; }
-            set { _timeScale = value; }
-        }
+        public float TimeScale { get; private set; }        
 
         public float DeltaTime
         {
-            get { return Time.deltaTime * _timeScale; }
+            get { return Time.deltaTime * TimeScale; }
         }
 
         public void PauseGame() => TimeScale = 0;
 
-        public void SpeedUpGame(int speedMultiplier) => TimeScale *= speedMultiplier;
+        public void SpeedUpGame(int speedMultiplier) => TimeScale = _defaultTimeScale * speedMultiplier;
 
         public void NormalTime() => TimeScale = 1;
     }
