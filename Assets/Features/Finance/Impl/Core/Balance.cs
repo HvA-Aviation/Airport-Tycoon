40564452e923ce94050f7f2263969b19ab823820
Joost <joostottens@hotmail.com>
2024-06-12 11:17:21 +0200

    Merged dev into this

--- Assets/Features/Finance/Impl/Core/Balance.cs
+++ Assets/Features/Finance/Impl/Core/Balance.cs
@@ -1,3 +1,6 @@
+using Features.EventManager;
+using Features.Managers;
+
 public class Balance
 {
     public int Value {get; private set;}
@@ -5,10 +8,12 @@ public class Balance
     public void Add(int amount) 
     {
         Value += amount;
+        GameManager.Instance.EventManager.TriggerEvent(EventId.OnMoneyAdded);
     }
     
     public void Subtract(int amount) 
     {
         Value -= amount;
+        GameManager.Instance.EventManager.TriggerEvent(EventId.OnMoneyRemoved);
     }
 }
