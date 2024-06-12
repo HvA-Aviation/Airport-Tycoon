90f498bda4fd8296e280f007df1b6f0efd8399f8
Stijn <shogerwerf.gaming@gmail.com>
2024-06-12 11:52:54 +0200

    Set random amount for SalaryAmount

--- Assets/Scripts/StaffManagement/Employee.cs
+++ Assets/Scripts/StaffManagement/Employee.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Employee : MonoBehaviour
 {
+    [SerializeField] private int _minimumSalary, _maximumSalary;
     public string Name { get; private set; }
     public int SalaryAmount { get; private set; }
     public int EmployeeID { get; private set; }
@@ -10,6 +11,7 @@ public class Employee : MonoBehaviour
     private void Awake()
     {
         Name = StaffNames.GetRandomFirstName() + " " + StaffNames.GetRandomLastName();
+        SalaryAmount = Random.Range(_minimumSalary, _maximumSalary);
     }
 
     public void SetEmployeeType(EmployeeTypes type) => EmployeeType = type;
