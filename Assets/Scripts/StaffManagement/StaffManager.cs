1c8fde39ffc7916e25f70f7353a2e29c152a9947
Stijn <shogerwerf.gaming@gmail.com>
2024-06-12 14:26:08 +0200

    Removed a debug.log

--- Assets/Scripts/StaffManagement/StaffManager.cs
+++ Assets/Scripts/StaffManagement/StaffManager.cs
@@ -35,7 +35,6 @@ public class StaffManager : MonoBehaviour
     {
         if(GameManager.Instance.FinanceManager.Balance.Value < _recruitmentCost)
         {
-            Debug.Log("Not enough money to hire");
             return;
         }
 
